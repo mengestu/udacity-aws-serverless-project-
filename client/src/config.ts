a8b4f34a5669f23c81dea1f0481fdb58a6698b27
mengestu <binwelbeck@gmail.com>
2022-10-03 11:38:47 +0300

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = 'lz0t779zj6';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`;
+
+export const authConfig = {
+  // Auth0 settings
+  domain: 'dev-0rvgf78o.us.auth0.com',
+  clientId: 'CAAt4X030qU2BFoDiWZghFcIOjAthqVO',
+  callbackUrl: 'http://localhost:3000/callback'
+};
